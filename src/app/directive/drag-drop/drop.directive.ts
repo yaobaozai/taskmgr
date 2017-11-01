ad69285302a0b117441ac10d3fd4bc0676520f25
yaobaozai <282926102@qq.com>
2017-11-01 20:01:41 +0800

    add

--- src/app/directive/drag-drop/drop.directive.ts
+++ src/app/directive/drag-drop/drop.directive.ts
@@ -1,10 +1,24 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, ElementRef, Renderer2, Input } from '@angular/core';
 
 @Directive({
   selector: '[app-droppable]'
 })
 export class DropDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef, private rd: Renderer2) { }
+
+  @HostListener('dragenter', ['$event'])
+  onDragEnter(ev: Event) {
+    if(this.el.nativeElement === ev.target){
+      this.rd.addClass(this.el.nativeElement, this.draggedClass);
+    }
+  }
+
+  @HostListener('dragend', ['$event'])
+  onDragEnd (ev: Event) {
+    if(this.el.nativeElement === ev.target) {
+      this.rd.removeClass(this.el.nativeElement, this.draggedClass);
+    }
+  }
 
 }
