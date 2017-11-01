18583a8e6748ec1cf919384ea12ee945b8311744
yaobaozai <282926102@qq.com>
2017-11-01 22:23:28 +0800

    add validate

--- src/app/directive/drag-drop/drop.directive.ts
+++ src/app/directive/drag-drop/drop.directive.ts
@@ -1,32 +1,11 @@
-<<<<<<< HEAD
-import { Directive, HostListener, ElementRef, Renderer2, Input } from '@angular/core';
-=======
 import { Directive, HostListener, ElementRef, Renderer2, Input, Output, EventEmitter } from '@angular/core';
 import { DragDropService, DragData } from '../drag-drop.service';
->>>>>>> ee3d4143277ba55e2e4c318d966e1d6bf29ff5e4
 
 @Directive({
   selector: '[app-droppable][dropTags][dragEnterClass]'
 })
 export class DropDirective {
 
-<<<<<<< HEAD
-  constructor(private el: ElementRef, private rd: Renderer2) { }
-
-  @HostListener('dragenter', ['$event'])
-  onDragEnter(ev: Event) {
-    if(this.el.nativeElement === ev.target){
-      this.rd.addClass(this.el.nativeElement, this.draggedClass);
-    }
-  }
-
-  @HostListener('dragend', ['$event'])
-  onDragEnd (ev: Event) {
-    if(this.el.nativeElement === ev.target) {
-      this.rd.removeClass(this.el.nativeElement, this.draggedClass);
-    }
-  }
-=======
   @Output() dropped = new EventEmitter<DragData>();
   @Input() dragEnterClass: string;
   @Input() dropTags: string[] = [];
@@ -97,6 +76,5 @@ export class DropDirective {
       })
     }    
   }
->>>>>>> ee3d4143277ba55e2e4c318d966e1d6bf29ff5e4
 
 }
--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+
+    this.form = this.fb.group({
+      email: ['282926102@qq.com', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.required]
+    })
+
+    this.form = new FormGroup({
+      email: new FormControl('282926102@qq.com', Validators.compose([Validators.required,Validators.email, this.validate])),
+      password: new FormControl('', Validators.required)
+    })
+  }
+
+  onSubmit({value, valid}, ev: Event) {
+    ev.preventDefault();
+    console.log(JSON.stringify(value));
+    console.log(valid);
+    // this.form.controls['email'].setValidators(this.validate);
+  }
+
+  validate(c: FormControl): {[key: string]: any} {
+    if(!c.value) {
+      return null;
+    }
+
+    const pattern = /^zeng+/;
+    if(pattern.test(c.value)) {
+      return null;
+    }
+    return {
+      emailNotValid: 'The email must start with zeng'
+    }
   }
 
 }
